1e72ae82efe3ba0c9e4d7a0b1a8a6538427142ce
Chris Kramer <ChrisKramer15@outlook.com>
2023-10-04 13:16:33 -0500

    color palette created for getaways

---
+++ getaways_flutter/lib/assets/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const mainColor = Colors.green;
+  static const darkShade = Colors.purple;
+  static const darkAccent = Colors.blue;
+  static const lightShade = Colors.red;
+  static const lightAccent = Colors.orange;
+}
