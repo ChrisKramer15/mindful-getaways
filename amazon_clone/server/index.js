3da88ab75f56c0ee729c3f010f0fe6cbe7133db6
Chris Kramer <ChrisKramer15@outlook.com>
2023-10-05 14:44:18 -0500

    successful server listen function, working on nodemon implementation

--- amazon_clone/server/index.js
+++ amazon_clone/server/index.js
@@ -1,1 +1,13 @@
-console.log("Hello, World")
+console.log("Hello, World");
+
+// CREATING AN API
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`connected to server at port ${PORT}`)
+})
+
+
+
