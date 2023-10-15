2b4cb5d3dbf5132cf047dc8122661b0b91cc0d04
Chris Kramer <ChrisKramer15@outlook.com>
2023-10-15 18:20:20 -0500

    background image successful

---
+++ getaways_flutter/lib/screens/auth_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("lib/assets/images/sunset_unsplash.jpg"),
+            fit: BoxFit.cover),
+      ),
+    ));
+  }
+}
